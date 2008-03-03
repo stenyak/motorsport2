84c44178dc5e27a2febc0feaf7f1f08057f1e916
stenyak <stenyak@localhost>
2008-03-03 21:22:55 +0100

    Added some missing UMl diagram files. Added helper scripts for Motorsport Simulator building.

---
+++ usr/share/motorsport-sim/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scons $* && LD_LIBRARY_PATH="../../lib/motorsport:." ./motorsport-simD && echo "--------------------" && gcov *.cpp |grep -e "File\ '[^\/].*" -A 1 && echo "--------------------" && grep -H "####" *.cpp.gcov |sed "s/\.gcov:/:/g;s/[\ ]*#####:[\ ]*//g;s/$$/  \/\/warning: line not covered\./g"
