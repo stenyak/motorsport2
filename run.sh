4e653e0489f35a9581626b87a25a7aa29ded13b2
stenyak <stenyak@localhost>
2008-04-09 21:26:29 +0200

    Fixed coverage testing routines.

--- run.sh
+++ run.sh
@@ -7,12 +7,23 @@ then
 else
     for i in $*
     do
-        bin=usr/bin/$i
-        echo Running $bin...
-        LD_LIBRARY_PATH="usr/lib:usr/lib/motorsport:usr/lib/motorsport-sim" $bin
-        #gcov usr/share/*/*.cpp |grep -e "File\ '[^\/].*" -A 1
-        gcov usr/share/*/*.cpp
-        #grep -H "####" usr/share/$i/*.cpp.gcov |sed "s/\.gcov:/:/g;s/[\ ]*#####:[\ ]*//g;s/$$/  \/\/warning: line not covered\./g"
+        echo ">>> Running $bin..."
+        LD_LIBRARY_PATH="usr/lib:usr/lib/motorsport:usr/lib/motorsport-sim" usr/bin/$i
+        for j in *.gc*; do rm "$j"; done
+        for j in usr/share/*/*.gcda
+        do
+            cp -f $(dirname "$j")/*.gc* .
+        done
+
+        echo ">>> Checking coverage..."
+        for j in usr/share/*/*.gcda
+        do
+            gcov -p $(dirname "$j")/*.cpp |grep -e "File\ '[^\/].*" -A 1 2>/dev/null 1>/dev/null
+        done
+
+        echo ">>> Showing lines..."
+        grep -H "####" *.gcov |grep "^usr#share" |sed "s/\.gcov:/:/g;s/\([0-9]\):/\1: warning: line not covered\./;s/[\ ]*#####:[\ ]*//g;s/#/\//g;s/\^/\.\./g"
+        rm *.gc*
         echo ""
     done
 fi
