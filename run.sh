68adb09c4e41c12a606f39f0448cb3818164359c
stenyak <stenyak@1ksurvivor>
2008-04-09 14:17:50 +0200

    More refactoring of build system.

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-if [ "$1" == "" ]
+if [ "$*" == "" ]
 then
     echo "Please specify a binary name from these:"
     echo -n "   "
     ls usr/bin
 else
-    LD_LIBRARY_PATH="usr/lib:usr/lib/motorsport:usr/lib/motorsport-sim" usr/bin/$1
+    for i in $*
+    do
+        bin=usr/bin/$i
+        echo Running $bin...
+        LD_LIBRARY_PATH="usr/lib:usr/lib/motorsport:usr/lib/motorsport-sim" $bin
+        #gcov usr/share/*/*.cpp |grep -e "File\ '[^\/].*" -A 1
+        gcov usr/share/*/*.cpp
+        #grep -H "####" usr/share/$i/*.cpp.gcov |sed "s/\.gcov:/:/g;s/[\ ]*#####:[\ ]*//g;s/$$/  \/\/warning: line not covered\./g"
+        echo ""
+    done
 fi
