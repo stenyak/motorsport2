b7e42e3424db0377e7cba915dc26449172a9bccc
stenyak <stenyak@1ksurvivor>
2008-04-12 03:29:56 +0200

    Removed a couple deprecated files.

--- usr/share/motorsport/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pushd .
-cd ../../..
-./mklinks.sh
-popd
-scons $* && LD_LIBRARY_PATH="../../lib/motorsport:." ./motorsport-tests && echo "--------------------" && gcov *.cpp |grep -e "File\ '[^\/].*" -A 1 && echo "--------------------" && grep -H "####" *.cpp.gcov |sed "s/\.gcov:/:/g;s/[\ ]*#####:[\ ]*//g;s/$$/  \/\/warning: line not covered\./g"
--- usr/share/thread-test/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-scons $* && LD_LIBRARY_PATH="../../lib/motorsport:." ./thread-test && echo "--------------------" && gcov *.cpp |grep -e "File\ '[^\/].*" -A 1 && echo "--------------------" && grep -H "####" *.cpp.gcov |sed "s/\.gcov:/:/g;s/[\ ]*#####:[\ ]*//g;s/$$/  \/\/warning: line not covered\./g"
