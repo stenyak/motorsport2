a60905526c4bc89772b2f5afbf4e418bb0ea94b7
stenyak <stenyak@1ksurvivor>
2008-07-08 18:45:19 +0200

    Made grabDeps scrit modular, the desired packages to be built can now be specified by hand.

--- grabDeps.sh
+++ grabDeps.sh
@@ -2,7 +2,8 @@
 
 cpus=$(cat /proc/cpuinfo  |grep processor |wc -l)
 
-function mkcd {
+function mkcd
+{
     if [ ! -d $1 ]
     then
         echo "Creating $1 directory..."
@@ -11,91 +12,122 @@ function mkcd {
     cd $1
 }
 
-mkcd deps
-    echo "==== Installing UnitTest++ ===="
-    echo " - Grabbing from svn..."
-    svn co https://unittest-cpp.svn.sourceforge.net/svnroot/unittest-cpp unittest-cpp
-    echo " - Building..."
-    cd unittest-cpp/UnitTest++
-        make -j $cpus
-    cd ../..
-    echo " - Copying libraries..."
-    mkdir -p ../usr/lib
-    cp unittest-cpp/UnitTest++/libUnitTest++.a ../usr/lib
-    echo " - Copying headers..."
-    mkdir -p ../usr/include/unittest++
-    cp -r unittest-cpp/UnitTest++/src/* ../usr/include/unittest++/
-    find ../usr/include/unittest++/ -name '*.[do]' -exec rm {} \;
-    find ../usr/include/unittest++/ -name '*.cpp' -exec rm {} \;
-    find ../usr/include/unittest++/ -type d -name '.svn' -exec rm -rf {} \;
-    echo "==== UnitTest++ installed ===="
-
-cd ..
-
-mkcd deps
-    echo "==== Installing SCons ===="
-    echo " - Grabbing from svn..."
-    svn checkout http://scons.tigris.org/svn/scons/trunk scons --username guest --password ""
-    echo " - Building..."
-    cd scons
-        export SCONS_LIB_DIR=`pwd`/src/engine
-        python src/script/scons.py build/scons
-        echo " - Copying files..."
-        cd build/scons
-            python setup.py install --prefix=../../../../usr
+function unittest
+{
+    mkcd deps
+        echo "==== Installing UnitTest++ ===="
+        echo " - Grabbing from svn..."
+        svn co https://unittest-cpp.svn.sourceforge.net/svnroot/unittest-cpp unittest-cpp
+        echo " - Building..."
+        cd unittest-cpp/UnitTest++
+            make -j $cpus
         cd ../..
+        echo " - Copying libraries..."
+        mkdir -p ../usr/lib
+        cp unittest-cpp/UnitTest++/libUnitTest++.a ../usr/lib
+        echo " - Copying headers..."
+        mkdir -p ../usr/include/unittest++
+        cp -r unittest-cpp/UnitTest++/src/* ../usr/include/unittest++/
+        find ../usr/include/unittest++/ -name '*.[do]' -exec rm {} \;
+        find ../usr/include/unittest++/ -name '*.cpp' -exec rm {} \;
+        find ../usr/include/unittest++/ -type d -name '.svn' -exec rm -rf {} \;
+        echo "==== UnitTest++ installed ===="
     cd ..
-    echo "==== SCons installed ===="
-cd ..
+}
 
-
-mkcd deps
-    echo "==== Installing FreeImage ===="
-    echo " - Grabbing from cvs..."
-    echo "Press enter to continue (empty password)."
-cvs -d:pserver:anonymous@freeimage.cvs.sourceforge.net:/cvsroot/freeimage login
-cvs -z3 -d:pserver:anonymous@freeimage.cvs.sourceforge.net:/cvsroot/freeimage co -P freeImage
-    echo " - Building..."
-    cd freeimage
-        # specify custom prefix by hand:
-        t=$(tempfile)
-        cat Makefile.gnu |sed "s/\/usr/\/\.\.\/\.\.\/usr/g;s/-.\ root//g" > $t; cp $t Makefile.gnu
-        rm $t
-        make -j $cpus
-        echo " - Copying files..."
-        make install
-    cd ..
-    echo "==== FreeImage installed ===="
-cd ..
-
-mkcd deps
-    echo "==== Installing Ogre ===="
-    echo " - Grabbing from svn..."
-    svn co https://svn.ogre3d.org/svnroot/ogre/trunk ogre
-    echo " - Building..."
-    cd ogre
-        ./bootstrap
-        ./configure --prefix=$PWD/../../usr
-        make -j $cpus
-        echo " - Copying files..."
-        make install
-    cd ..
-    echo "==== Ogre installed ===="
-cd ..
-
-mkcd deps
-    echo "==== Installing OgreCollada ===="
-    echo " - Grabbing from svn..."
-    svn co https://ogrecollada.svn.sourceforge.net/svnroot/ogrecollada/trunk ogrecollada
-    echo " - Building..."
-    cd ogrecollada
-        cd premake
-            PKG_CONFIG_PATH="$PWD/../../../usr/lib/pkgconfig" premake --target gnu
-            PKG_CONFIG_PATH="$PWD/../../../usr/lib/pkgconfig" make -j $cpus
+function scons
+{
+    mkcd deps
+        echo "==== Installing SCons ===="
+        echo " - Grabbing from svn..."
+        svn checkout http://scons.tigris.org/svn/scons/trunk scons --username guest --password ""
+        echo " - Building..."
+        cd scons
+            export SCONS_LIB_DIR=`pwd`/src/engine
+            python src/script/scons.py build/scons
+            echo " - Copying files..."
+            cd build/scons
+                python setup.py install --prefix=../../../../usr
+            cd ../..
         cd ..
-        echo " - Copying files..."
-        cp bin/release/lib* ../../usr/lib
-        cp OgreCollada/OgreCollada/include/*.h ../../usr/include
+        echo "==== SCons installed ===="
     cd ..
-    echo "==== OgreCollada installed ===="
-cd ..
+}
+
+function freeimage
+{
+    mkcd deps
+        echo "==== Installing FreeImage ===="
+        echo " - Grabbing from cvs..."
+        echo "Press enter to continue (empty password)."
+    cvs -d:pserver:anonymous@freeimage.cvs.sourceforge.net:/cvsroot/freeimage login
+    cvs -z3 -d:pserver:anonymous@freeimage.cvs.sourceforge.net:/cvsroot/freeimage co -P freeImage
+        echo " - Building..."
+        cd freeimage
+            # specify custom prefix by hand:
+            t=$(tempfile)
+            cat Makefile.gnu |sed "s/\/usr/\/\.\.\/\.\.\/usr/g;s/-.\ root//g" > $t; cp $t Makefile.gnu
+            rm $t
+            make -j $cpus
+            echo " - Copying files..."
+            make install
+        cd ..
+        echo "==== FreeImage installed ===="
+    cd ..
+}
+
+function ogre
+{
+    mkcd deps
+        echo "==== Installing Ogre ===="
+        echo " - Grabbing from svn..."
+        svn co https://svn.ogre3d.org/svnroot/ogre/trunk ogre
+        echo " - Building..."
+        cd ogre
+            ./bootstrap
+            ./configure --prefix=$PWD/../../usr
+            make -j $cpus
+            echo " - Copying files..."
+            make install
+        cd ..
+        echo "==== Ogre installed ===="
+    cd ..
+}
+
+function ogrecollada
+{
+    mkcd deps
+        echo "==== Installing OgreCollada ===="
+        echo " - Grabbing from svn..."
+        svn co https://ogrecollada.svn.sourceforge.net/svnroot/ogrecollada/trunk ogrecollada
+        echo " - Building..."
+        cd ogrecollada
+            cd premake
+                PKG_CONFIG_PATH="$PWD/../../../usr/lib/pkgconfig" premake --verbose --target gnu
+                PKG_CONFIG_PATH="$PWD/../../../usr/lib/pkgconfig" make #-j $cpus
+            cd ..
+            echo " - Copying files..."
+            cp bin/release/lib* ../../usr/lib
+            cp OgreCollada/OgreCollada/include/*.h ../../usr/include
+        cd ..
+        echo "==== OgreCollada installed ===="
+    cd ..
+}
+
+params="unittest scons freeimage ogre ogrecollada"
+
+if [ "$*" != "" ]; then
+    params="$*"
+fi
+
+for i in $params
+do
+    case "$i" in
+        "unittest") unittest;;
+        "scons") scons;;
+        "freeimage") freeimage;;
+        "ogre") ogre;;
+        "ogrecollada") ogrecollada;;
+        *) ;;
+    esac
+done
